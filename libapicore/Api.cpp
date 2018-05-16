054a702d3010f1ff4f4cd9b138dfd03fe160b219
ShijieXu <shijiexu@yahoo.com>
2018-05-17 00:31:58 +0800

    record beast error link

--- libapicore/Api.cpp
+++ libapicore/Api.cpp
@@ -1,4 +1,6 @@
 #include "Api.h"
+
+#include <boost/beast.hpp>
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/beast/core.hpp>
@@ -68,12 +70,13 @@ bool HttpApi::postData(string json)
         // Make the connection on the IP address we get from a lookup
         boost::asio::connect(socket, results.begin(), results.end());
 
-        // Set up an HTTP GET request message
         http::request<http::string_body> req{http::verb::post, url, 11};
         req.set(http::field::host, remoteHost);
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-		req.set(http::field::content_type, "application/json; charset=utf-8");
-		req.set(http::field::body, json);
+        req.set(http::field::content_type, "application/json; charset=utf-8");
+        //req.set(http::field::content_length, json.size());
+		req.body() = json;
+        req.prepare_payload();
 
         // Send the HTTP request to the remote host
         http::write(socket, req);
